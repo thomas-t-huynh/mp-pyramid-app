8589a902f9baea94dd416fccf860eee2c3820493
thomas <t_huynh927@yahoo.com>
2020-02-22 17:59:28 -0800

    first commit, created login for app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+
+import Login from "./components/Login";
+
 import './App.css';
 
 function App() {
+  const [ userData, setUserData ] = useState()
+  console.log(userData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route exact path="/user/login" render={props => <Login {...props} setUserData={setUserData} />} />
+
     </div>
   );
 }
